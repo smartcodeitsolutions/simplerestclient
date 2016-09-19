6b30ebadfc7402748cec8e81d8884bc5c815f844
Ramiro <Ramiro@DESKTOP-80EHBCU>
2016-09-19 16:02:41 -0500

    Look and feel changes

--- SimpleRestClient/RestToolWindowControl.xaml.cs
+++ SimpleRestClient/RestToolWindowControl.xaml.cs
@@ -6,8 +6,13 @@
 
 namespace SimpleRestClient
 {
+    using System;
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Reflection;
     using System.Text;
     using System.Windows;
     using System.Windows.Controls;
@@ -18,12 +23,48 @@ namespace SimpleRestClient
     public partial class RestToolWindowControl : UserControl
     {
         static HttpClient httpClient = new HttpClient();
+        string selectedMethod = "GET";
+        HttpMethod choosedMethod = HttpMethod.Get;
+
+
+        public static bool SetAllowUnsafeHeaderParsing20()
+        {
+            //Get the assembly that contains the internal class
+            Assembly aNetAssembly = Assembly.GetAssembly(typeof(System.Net.Configuration.SettingsSection));
+            if (aNetAssembly != null)
+            {
+                //Use the assembly in order to get the internal type for the internal class
+                Type aSettingsType = aNetAssembly.GetType("System.Net.Configuration.SettingsSectionInternal");
+                if (aSettingsType != null)
+                {
+                    //Use the internal static property to get an instance of the internal settings class.
+                    //If the static instance isn't created allready the property will create it for us.
+                    object anInstance = aSettingsType.InvokeMember("Section",
+                      BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.NonPublic, null, null, new object[] { });
+
+                    if (anInstance != null)
+                    {
+                        //Locate the private bool field that tells the framework is unsafe header parsing should be allowed or not
+                        FieldInfo aUseUnsafeHeaderParsing = aSettingsType.GetField("useUnsafeHeaderParsing", BindingFlags.NonPublic | BindingFlags.Instance);
+                        if (aUseUnsafeHeaderParsing != null)
+                        {
+                            aUseUnsafeHeaderParsing.SetValue(anInstance, true);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="RestToolWindowControl"/> class.
         /// </summary>
         public RestToolWindowControl()
         {
             this.InitializeComponent();
+            SetAllowUnsafeHeaderParsing20();
+            
         }
 
         /// <summary>
@@ -45,44 +86,90 @@ namespace SimpleRestClient
 
         }
 
-        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Add a new Request Message
+            HttpRequestMessage requestMessage = new HttpRequestMessage(choosedMethod, urlTextBox.Text.Trim());
+            string data;
+            string headerString = string.Empty;
+            string headerValues = string.Empty;
+            // Add our custom headers
+            requestMessage.Headers.Add("User-Agent", "User-Agent-Here");
+
+
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Send the request to the server
             try
             {
-                HttpResponseMessage r;
-                string data;
-                switch (((ListBoxItem)methodComboBox.SelectedValue).Content.ToString())
+                HttpResponseMessage r = await httpClient.SendAsync(requestMessage);
+                data = await r.Content.ReadAsStringAsync();
+                responseTextBox.Text = data;
+                foreach (var header in r.Content.Headers)
                 {
-                    case "POST":
+                    headerValues = string.Empty;
+                    foreach (var item in header.Value)
+                    {
+                        headerValues += item;
+                    }
 
-                        r = await httpClient.PostAsync(urlTextBox.Text.Trim(), new StringContent(sendTextBox.Text.Trim(), Encoding.UTF8, "application/json"));
-                        data = await r.Content.ReadAsStringAsync();
-                        responseTextBox.Text = data;
-                        break;
-                    case "GET":
-                        r = await httpClient.GetAsync(urlTextBox.Text.Trim(), HttpCompletionOption.ResponseContentRead);
-                        data = await r.Content.ReadAsStringAsync();
-                        responseTextBox.Text = data;
-                        break;
-                    case "PUT":
-                        r = await httpClient.PutAsync(urlTextBox.Text.Trim(), new StringContent(sendTextBox.Text.Trim(), Encoding.UTF8, "application/json"));
-                        data = await r.Content.ReadAsStringAsync();
-                        responseTextBox.Text = data;
-                        break;
-                    case "DELETE":
-                        r = await httpClient.DeleteAsync(urlTextBox.Text.Trim());
-                        data = await r.Content.ReadAsStringAsync();
-                        responseTextBox.Text = data;
-                        break;
-                    default:
-                        break;
+                    headerString += $"{header.Key}: {headerValues}\n";
                 }
-            }
-            catch (System.Exception ex)
-            {
-                MessageBox.Show("The operation cannot be completed.");
-            }
 
+                foreach (var header in r.Headers)
+                {
+                    headerValues = string.Empty;
+                    foreach (var item in header.Value)
+                    {
+                        headerValues += item;
+                    }
+
+                    headerString += $"{header.Key}: {headerValues}\n";
+                }
+
+                headersTextBox.Text = headerString;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The operation cannot be completed." + ex.Message);
+            }
+        
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked.Value)
+                //selectedMethod = "GET"; 
+                choosedMethod = HttpMethod.Get;
+        }
+
+        private void radioButton_Copy_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked.Value)
+                //selectedMethod = "POST";
+                choosedMethod = HttpMethod.Post;
+        }
+
+        private void radioButton_Copy1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked.Value)
+                //selectedMethod = "PUT";
+                choosedMethod = HttpMethod.Put;
+        }
+
+        private void radioButton_Copy2_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked.Value)
+                //selectedMethod = "DELETE";
+                choosedMethod = HttpMethod.Delete;
+        }
+
+        private void radioButton_Copy3_Checked(object sender, RoutedEventArgs e)
+        {
+            if (((RadioButton)sender).IsChecked.Value)
+                //selectedMethod = "OPTIONS";
+                choosedMethod = HttpMethod.Options;
         }
     }
 }
--- SimpleRestClient/RestToolWindowPackage.cs
+++ SimpleRestClient/RestToolWindowPackage.cs
@@ -38,9 +38,10 @@ namespace SimpleRestClient
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideToolWindow(typeof(RestToolWindow), Style = VsDockStyle.Tabbed, Window = EnvDTE.Constants.vsWindowKindSolutionExplorer, Orientation = ToolWindowOrientation.Right)]
+    [ProvideToolWindow(typeof(RestToolWindow), Style = VsDockStyle.Tabbed, Window = EnvDTE.Constants.vsWindowKindMainWindow, Orientation = ToolWindowOrientation.Right)]
     [Guid(RestToolWindowPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideBindingPath]
     public sealed class RestToolWindowPackage : Package
     {
         /// <summary>
